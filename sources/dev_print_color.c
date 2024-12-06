f7e3ef84159146dfab5787aaff3b2fe70c0ce561
Linda Lacsivy <lindalacsivy@outlook.de>
2024-12-06 13:39:16 +0100

    dev_prints removed

--- sources/dev_print_color.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "../includes/color.h"
-
-void	print_color(t_color color)
-{
-	printf("color: RED %f, GREEN %f, BLUE %f\n", color.colors_float[RED], color.colors_float[GREEN], color.colors_float[BLUE]);
-}
